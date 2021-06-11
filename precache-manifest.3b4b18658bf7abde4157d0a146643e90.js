6eb48c1fa94e123d5ca0c71d707a5470b1308dfd
TharangaPremathilaka <93218@fhss.sjp.ac.lk>
2021-06-11 20:28:44 +0530

    Updates

---
+++ precache-manifest.3b4b18658bf7abde4157d0a146643e90.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3263d6e1f3bac39d7cee813bc213eedc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fea69b7d3e91e72b87e2",
+    "url": "/static/css/main.a90790e1.chunk.css"
+  },
+  {
+    "revision": "94833f0d9aaa3be6210f",
+    "url": "/static/js/2.19e99e5d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.19e99e5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fea69b7d3e91e72b87e2",
+    "url": "/static/js/main.c7cc25d5.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "8db1995eecff673c67e34eb2a8d0e184",
+    "url": "/static/media/osh.8db1995e.jpg"
+  },
+  {
+    "revision": "37cfc1c7ff83904a440e5059cd6b15c3",
+    "url": "/static/media/resume.37cfc1c7.pdf"
+  }
+]);
